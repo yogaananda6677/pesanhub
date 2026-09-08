493d79eae9baf3dce932b3314ec7eb0a5aa7d4fe
Yoga Ananda <yogaanandaxx1212@gmail.com>
2026-09-08 10:26:29 +0700

    feat(mobile): dukung koneksi LAN lokal, feedback responsif, dan indikator kepedasan (#143) (#144)
    * feat(mobile): dukung koneksi LAN lokal, feedback responsif, dan indikator kepedasan (#143)
    
    * style(mobile): rapikan format kode menu_item.dart

--- pesenhub_app/lib/menu/models/menu_item.dart
+++ pesenhub_app/lib/menu/models/menu_item.dart
@@ -32,6 +32,10 @@ class MenuItem {
   /// True if item has customizable modifier groups.
   bool get hasModifiers => modifierGroups.isNotEmpty;
 
+  /// True if item has spice level modifier group.
+  bool get hasSpiceLevel =>
+      modifierGroups.any((g) => g.code == 'spice_level' && g.isActive);
+
   /// Returns active modifier groups.
   List<MenuModifierGroup> get activeModifierGroups =>
       modifierGroups.where((g) => g.isActive).toList();
